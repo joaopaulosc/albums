2665add6ff3cbcb65a720e741c19b3f268bfaad4
João Paulo S. Correia <joaopaulosc@gmail.com>
2019-01-08 13:47:38 -0300

    Primeiro commit

---
+++ src/components/header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+    const { textStyle, viewStyle } = styles;
+
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>{props.headerText}</Text>
+        </View>
+    );
+}
+
+const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 20 },
+        shadowOpacity: 0.8,
+        elevation: 1
+    },
+    textStyle: {
+        fontSize: 20
+    }
+}
+
+export default Header;
